ed16a2f344741f005d8afc6824f659cbb289e322
ALAN SUNNY <alans.sunny@gmail.com>
2023-01-22 22:46:44 +0530

    fr

--- assets/js/chevron.js
+++ assets/js/chevron.js
@@ -5,3 +5,24 @@
 function changechevron(id) {
     document.getElementById(id).classList.toggle('rotatechevron');
 }
+
+// $('.dropdown').on('mouseover',()=>{
+//     $('.dropdown-parent').addClass('show');
+// })
+
+$('.dropdown-menu a.dropdown-toggle').on('mouseover', function(e) {
+    if (!$(this).next().hasClass('show')) {
+      $(this).parents('.dropdown-menu').first().find('.show').removeClass('show');
+    }
+    var $subMenu = $(this).next('.dropdown-menu');
+    $subMenu.toggleClass('show');
+  
+  
+    $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+      $('.dropdown-submenu .show').removeClass('show');
+    });
+  
+  
+    return false;
+  });
+
