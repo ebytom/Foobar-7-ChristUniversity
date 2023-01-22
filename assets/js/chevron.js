56690215495a8fc1724453c9cb26df55bde7176f
ALAN SUNNY <alans.sunny@gmail.com>
2023-01-22 20:26:12 +0530

    fwe

--- assets/js/chevron.js
+++ assets/js/chevron.js
@@ -1,13 +1,3 @@
-function changechevron(id){
-    var elem = document.getElementById(id);
-    var clarr = elem.getAttribute('class').split(" ");
-    if(clarr.indexOf('rotatechevron')>0){
-        clarr.pop('rotatechevron')
-        clarr.join(" ");
-        elem.setAttribute('class',clarr);
-    }else{
-        clarr.push('rotatechevron')
-        clarr.join(" ");
-        elem.setAttribute('class',clarr);
-    }
-}
+$(".chevron").click(function(){
+    $(this).toggleClass("rotatechevron"); 
+});
