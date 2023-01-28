65b14bf98f59bf13b39731b26e41e2b24877c29d
EBY TOM <ebytomy7@gmail.com>
2023-01-28 09:30:16 +0530

    jhb

--- assets/js/eventList.js
+++ assets/js/eventList.js
@@ -1,195 +1,213 @@
-const events ={
-    "four": {
-        "id":"inauguration",
-    },
-    "five": {
-        "id":"holiday"
-    },
-    "six": {
-        "id":"openmic",
-        "name":"OPEN MIC",
-        "time":"4 PM - 6 PM",
-        "venue":"Open Audi",
-        "register":"",
-        "more":""
-    },
-    "seven": {
-        "id":"ai",
-        "name":"AI HACKATHON",
-        "time":"4 PM - 6 PM",
-        "venue":"",
-        "register":"",
-        "more":""
-    },
-    "eight": {
-        "id":"query",
-        "name":"BREAK THE QUERY",
-        "time":"4 PM - 6 PM",
-        "venue":"",
-        "register":"",
-        "more":""
-    },
-    "nine":{
-        "id":"codeathon",
-        "name":"CODEATHON",
-        "time":"4 PM - 6 PM",
-        "venue":"",
-        "register":"",
-        "more":""
-    },
-    "ten": {
-        "webally":{
-            "id":"webally",
-            "name":"WEBALLY",
-            "time":"1 PM",
-            "venue":"",
-            "register":"",
-            "more":""
-        },
-        "blind":{
-            "id":"blind",
-            "name":"BLIND TYPING",
-            "time":"2 PM - 4 PM",
-            "venue":"",
-            "register":"",
-            "more":""
-        },
-        "ideathon":{
-            "id":"ideathon",
-            "name":"IDEATHIN",
-            "time":"4 PM - 6 PM",
-            "venue":"",
-            "register":"",
-            "more":""
-        }
-    },
-    "eleven": {
-        "id":"ace",
-        "name":"ACE CLUTCH",
-        "time":"10 PM - 4 PM",
-        "venue":"",
-        "register":"",
-        "more":""
-    },
-    "twelve":{
-        "id":"holiday"
-    },
-    "thirteen": {
-        "id":"image",
-        "name":"IMAGE BRUSH",
-        "time":"4 PM - 6 PM",
-        "venue":"",
-        "register":"",
-        "more":""
-    },
-    "fourteen": {
-        "short":{
-            "id":"short",
-            "name":"SHORT GAME DEVELOPMENT",
-            "time":"2 PM - 4 PM",
-            "venue":"",
-            "register":"",
-            "more":""
-        },
-        "flix":{
-            "id":"flix",
-            "name":"FLIX n MANGA",
-            "time":"4 PM - 6 PM",
-            "venue":"",
-            "register":"",
-            "more":""
-        }
-    },
-    "fifteen":{
-        "id":"illustra",
-            "name":"ILLUSTRA",
-            "time":"4 PM - 6 PM",
-            "venue":"",
-            "register":"",
-            "more":""
-    },
-    "sixteen": {
-        "id":"css",
-            "name":"CSS BATTLE",
-            "time":"4 PM - 6 PM",
-            "venue":"",
-            "register":"",
-            "more":""
-    },
-    "seventeen": {
-        "id":"quiz",
-            "name":"TECH QUIZ",
-            "time":"2 PM - 4 PM",
-            "venue":"OPen Audi",
-            "register":"",
-            "more":""
-    },
-    "eighteen":{
-        "id":"holiday"
-    },
-    "nineteen":{
-        "id":"holiday"
-    },
-    "twenty":{
-        "id":"accio",
-        "name":"ACCIO LA NOCIEN",
-        "time":"4 PM - 6 PM",
-        "venue":"",
-        "register":"",
-        "more":""
-    },
-    "twentyone":{
-        "id":"Workshop"
-    },
-    "twentytwo":{
-        "id":"java",
-        "name":"JAVA WORKSHOP",
-        "time":"",
-        "venue":"",
-        "register":"",
-        "more":""
-    },
-    "twentythree":{
-        "id":"java",
-        "name":"JAVA WORKSHOP",
-        "time":"",
-        "venue":"",
-        "register":"",
-        "more":""
-    },
-    "twentyfour":{
-        "id":"c",
-        "name":"C WORKSHOP",
-        "time":"",
-        "venue":"",
-        "register":"",
-        "more":""
-    },
-    "twentyfive":{
-        "id":"c",
-        "name":"C WORKSHOP",
-        "time":"",
-        "venue":"",
-        "register":"",
-        "more":""
-    },
-    "twentysix":{
-        "id":"holiday",
-    },
-    "twentyseven":{
-        "id":"web",
-        "name":"WEB DEVELOPMENT WORKSHOP",
-        "time":"",
-        "venue":"",
-        "register":"",
-        "more":""
-    },
-    "twentyfive":{
-        "id":"web",
-        "name":"WEB DEVELOPMENT WORKSHOP",
-        "time":"",
-        "venue":"",
-        "register":"",
-        "more":""
-    }
+const eventList={
+    "four": [
+      {
+        "id": "inauguration",
+        "name": "INAUGURATION"
+      }
+    ],
+    "six": [
+      {
+        "id": "openmic",
+        "name": "OPEN MIC",
+        "time": "4 PM - 6 PM",
+        "venue": "Open Audi",
+        "register": "",
+        "more": ""
+      }
+    ],
+    "seven": [
+      {
+        "id": "ai",
+        "name": "AI HACKATHON",
+        "time": "4 PM - 6 PM",
+        "venue": "",
+        "register": "",
+        "more": ""
+      }
+    ],
+    "eight": [
+      {
+        "id": "query",
+        "name": "BREAK THE QUERY",
+        "time": "4 PM - 6 PM",
+        "venue": "",
+        "register": "",
+        "more": ""
+      }
+    ],
+    "nine": [
+      {
+        "id": "codeathon",
+        "name": "CODEATHON",
+        "time": "4 PM - 6 PM",
+        "venue": "",
+        "register": "",
+        "more": ""
+      }
+    ],
+    "ten": [
+      {
+        "id": "webally",
+        "name": "WEBALLY",
+        "time": "1 PM",
+        "venue": "",
+        "register": "",
+        "more": ""
+      },
+      {
+        "id": "blind",
+        "name": "BLIND TYPING",
+        "time": "2 PM - 4 PM",
+        "venue": "",
+        "register": "",
+        "more": ""
+      },
+      {
+        "id": "ideathon",
+        "name": "IDEATHIN",
+        "time": "4 PM - 6 PM",
+        "venue": "",
+        "register": "",
+        "more": ""
+      }
+    ],
+    "eleven": [
+      {
+        "id": "ace",
+        "name": "ACE CLUTCH",
+        "time": "10 PM - 4 PM",
+        "venue": "",
+        "register": "",
+        "more": ""
+      }
+    ],
+    "thirteen": [
+      {
+        "id": "image",
+        "name": "IMAGE BRUSH",
+        "time": "4 PM - 6 PM",
+        "venue": "",
+        "register": "",
+        "more": ""
+      }
+    ],
+    "fourteen": [
+      {
+        "id": "short",
+        "name": "SHORT GAME DEVELOPMENT",
+        "time": "2 PM - 4 PM",
+        "venue": "",
+        "register": "",
+        "more": ""
+      },
+      {
+        "id": "flix",
+        "name": "FLIX n MANGA",
+        "time": "4 PM - 6 PM",
+        "venue": "",
+        "register": "",
+        "more": ""
+      }
+    ],
+    "fifteen": [
+      {
+        "id": "illustra",
+        "name": "ILLUSTRA",
+        "time": "4 PM - 6 PM",
+        "venue": "",
+        "register": "",
+        "more": ""
+      }
+    ],
+    "sixteen": [
+      {
+        "id": "css",
+        "name": "CSS BATTLE",
+        "time": "4 PM - 6 PM",
+        "venue": "",
+        "register": "",
+        "more": ""
+      }
+    ],
+    "seventeen": [
+      {
+        "id": "quiz",
+        "name": "TECH QUIZ",
+        "time": "2 PM - 4 PM",
+        "venue": "OPen Audi",
+        "register": "",
+        "more": ""
+      }
+    ],
+    "twenty": [
+      {
+        "id": "accio",
+        "name": "ACCIO LA NOCIEN",
+        "time": "4 PM - 6 PM",
+        "venue": "",
+        "register": "",
+        "more": ""
+      }
+    ],
+    "twentyone": [
+      {
+        "id": "Workshop"
+      }
+    ],
+    "twentytwo": [
+      {
+        "id": "java",
+        "name": "JAVA WORKSHOP",
+        "time": "",
+        "venue": "",
+        "register": "",
+        "more": ""
+      }
+    ],
+    "twentythree": [
+      {
+        "id": "java",
+        "name": "JAVA WORKSHOP",
+        "time": "",
+        "venue": "",
+        "register": "",
+        "more": ""
+      }
+    ],
+    "twentyfour": [
+      {
+        "id": "c",
+        "name": "C WORKSHOP",
+        "time": "",
+        "venue": "",
+        "register": "",
+        "more": ""
+      }
+    ],
+    "twentyfive": [
+      {
+        "id": "web",
+        "name": "WEB DEVELOPMENT WORKSHOP",
+        "time": "",
+        "venue": "",
+        "register": "",
+        "more": ""
+      }
+    ],
+    "twentyseven": [
+      {
+        "id": "web",
+        "name": "WEB DEVELOPMENT WORKSHOP",
+        "time": "",
+        "venue": "",
+        "register": "",
+        "more": ""
+      }
+    ]
   }
+
+
+  eventList[v].map(eventList=>{
+    var elem = `<div class="elecard">${event}</div>`
+    document.getElementById('childdiv1').innerHTML += elem;
+  })
