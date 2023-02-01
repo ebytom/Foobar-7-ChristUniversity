7705e6c85ad28bc60a93d67817b5f844289633ff
EBY TOM <ebytomy7@gmail.com>
2023-02-01 22:24:03 +0530

    hdb

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,23 +1,21 @@
 // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-analytics.js";
-// import firebase from "firebase/app";
-// import "firebase/analytics";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyBDOoAg7rQ-_e5dduEG3-FX7XvQPiiNQ28",
-    authDomain: "foobar-956a3.firebaseapp.com",
-    projectId: "foobar-956a3",
-    storageBucket: "foobar-956a3.appspot.com",
-    messagingSenderId: "906024644766",
-    appId: "1:906024644766:web:72bd2a657f58c4b6ddcb8f"
-  };
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBA4qRVhHlm5dRUENu-vywgsTBnRIp0mH8",
+  authDomain: "foobar-analytics.firebaseapp.com",
+  projectId: "foobar-analytics",
+  storageBucket: "foobar-analytics.appspot.com",
+  messagingSenderId: "479667898600",
+  appId: "1:479667898600:web:64744194442d8a8b4f615d",
+  measurementId: "G-EWPJCH9BFG"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-// firebase.initializeApp(firebaseConfig);
-// const analytics = firebase.analytics();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
